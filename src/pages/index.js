20f44e3cd811f51e2fe894d2e10523b795b454b9
Darrell Little <darrell.little@gmail.com>
2019-05-31 02:35:08 -0400

    Add CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,9 @@ export default () => (
     <div style={{ color: "purple"}}>
     <Header headerText="Well, Hello world!" />
     <h3>This is Gatsby!</h3>
+    <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
     <p>Are we having fun yet?</p>
-    <p><img src="https://source.unsplash.com/random/400x200" alt="" /></p>
+    <p><img src="https://source.unsplash.com/random/400x200" alt="random" width="400" height="200" /></p>
     <p><Link to="/contact/">Contact</Link></p>
 </div>
 ) 
