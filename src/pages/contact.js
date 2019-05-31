f18449479732794359c358a46a3347aa36336a12
Darrell Little <darrell.little@gmail.com>
2019-05-31 17:47:10 -0400

    Update DIV and Images

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import logo from "../../static/photo.jpg"
 
 export default () => (
     <div>
         <Header headerText="Contact Me" />
         <p>Email: darrell (at) nokecodes (dot) org</p>
-        <p><img src="https://source.unsplash.com/random/400x200" alt="" /></p>
-        <p><Link to="/">Home</Link></p>
+        <p><img src={logo} alt="Random Photo" /></p>
+        <p><Link to="/"><button>Home</button></Link></p>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import logo from "../../static/undraw_clean_up.svg"
 
 export default () => (
     <div>
-    <Header headerText="Well, Hello world!" />
-    <h3>This is Gatsby!</h3>
-    <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
-    <p>Are we having fun yet?</p>
-    <p><img src="https://source.unsplash.com/random/400x200" alt="random" width="400" height="200" /></p>
-    <p><Link to="/contact/">Contact</Link></p>
-</div>
+        <Header headerText="Well, Hello world!" />
+        <h3>This is Gatsby!</h3>
+        <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
+        <p>Are we having fun yet?</p>
+        <p><img src={logo} width="220" /></p>
+        <p><Link to="/contact/"><button>Contact</button></Link></p>
+    </div>
 ) 
