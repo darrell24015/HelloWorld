c6080f40081383d252f2b2ee2794fadf9d1b9aec
Darrell Little <darrell.little@gmail.com>
2019-06-01 00:06:03 -0400

    Add new image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 import logo from "../../static/undraw_clean_up.svg"
+import monogram from "../../static/monogram.svg"
 
 export default () => (
     <div>
-        <Header headerText="Well, Hello world!" />
+        <Header headerText="Well, Hello There!" />
         <h3>This is Gatsby!</h3>
-        <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
+        <img alt="Gatsby" src={monogram} width="60" />
         <p>Are we having fun yet?</p>
-        <p><img src={logo} width="220" /></p>
+        <p><img src={logo} width="220" alt="" /></p>
         <p><Link to="/contact/"><button>Contact</button></Link></p>
     </div>
 ) 
